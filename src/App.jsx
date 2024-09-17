6f75c34132740895b7f8b7eebcae00064f8ae86f
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2024-09-17 16:01:43 +0530

    initial2

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(10)
+  
 
   return (
     <>
       Gemini clone
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet error, voluptates ipsa nisi soluta assumenda modi rerum accusamus magni possimus enim nesciunt ea molestiae, voluptatibus necessitatibus nemo doloribus optio dolor?
+      <h1>Lokesh</h1>
+      <p>loerj</p>
+      gkoala
+      {1+32}
+      {count}
     </>
   )
 }
