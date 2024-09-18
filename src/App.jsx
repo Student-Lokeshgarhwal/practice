d6ec9c9e82ff4e475522b02a756b6c26feaa319c
Student-Lokeshgarhwal <lokeshgarhwal1805@gmail.com>
2024-09-18 19:59:16 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       Gemini clone
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet error, voluptates ipsa nisi soluta assumenda modi rerum accusamus magni possimus enim nesciunt ea molestiae, voluptatibus necessitatibus nemo doloribus optio dolor?
       <h1>Lokesh</h1>
+      <h1>Garhwal</h1>
       <p>loerj</p>
       gkoala
       {1+32}
